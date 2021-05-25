d59e3663c31ed05c38fffcccb9644918d6bd74c5
VladimirGavrilov <gavriloff.v@yandex.ru>
2021-05-26 00:58:50 +0300

    add style

--- src/App.js
+++ src/App.js
@@ -5,19 +5,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        header
       </header>
+      <div className="App-content">
+        <div className="App-content-left">
+          <h2>menu</h2>
+          <ul>
+            <li><div><a href="/"><h5>1 menu</h5></a></div></li>
+            <li><div><a href="/"><h5>2 menu</h5></a></div></li>
+            <li><div><a href="/"><h5>3 menu</h5></a></div></li>
+            <li><div><a href="/"><h5>4 menu</h5></a></div></li>
+            <li><div><a href="/"><h5>5 menu</h5></a></div></li>
+            
+          </ul>
+        </div>
+        <div className="App-content-right">
+          <div className="App-content-center">
+            <div><h3>CARD CENTER CONTENT</h3></div>
+            <div><h3>CARD CENTER CONTENT</h3></div>
+            <div><h3>CARD CENTER CONTENT</h3></div>
+            <div><h3>CARD CENTER CONTENT</h3></div>
+            
+          </div>
+          <div className="App-content-bottom">
+            <h2>BOTTOM CONTENT</h2>
+          </div>
+        </div>
+      </div>      
+      <div className="App-footer">
+        <h2>FOOTER CONTENT</h2>
+      </div>
     </div>
   );
 }
